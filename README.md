e7f5b5f52e1eacf4a01f7ef286832a26289e67e6
James Anh <james.anh184@gmail.com>
2022-05-31 22:23:59 -0700

    debugged some features

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Work-Day-Schedule
+## Acceptance Criteria
+- GIVEN I am using a daily planner to create a schedule
+- WHEN I open the planner
+- THEN the current day is displayed at the top of the calendar
+- WHEN I scroll down
+- THEN I am presented with time blocks for standard business hours
+- WHEN I view the time blocks for that day
+- THEN each time block is color-coded to indicate whether it is in the past, present, or future
+- WHEN I click into a time block
+- THEN I can enter an event
+- WHEN I click the save button for that time block
+- THEN the text for that event is saved in local storage
+- WHEN I refresh the page
+- THEN the saved events persist
+
+# App Description
+
+# Repository & Deploy URL
+-Repository link: https://github.com/JamesAHo/QuizApplication <br>
+-Deployed Link: https://jamesaho.github.io/QuizApplication/
+# Mock up
+
+
--- app.js
+++ app.js
@@ -1,49 +1,51 @@
-var todayDate = moment().format('ddd, MMM Do YYYY');
-// use jquery to write html elements
+// Make variable to get current time using moment().format().
+var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
-// use documenet.ready() to manipulate document safetly.
+$(document).ready(function () {
+    // add event on the saved button.
+    // When clicked on saved button, infomation will be saved into localstorage.
+    $(".saveBtn").on("click", function () {
+        // 
+        var Plan = $(this).siblings(".description").val();
+        var clock = $(this).parent().attr("id");
 
-$(document).ready(function() {
-    // console.log("document loaded!");
-    // function for savedButton
-    $(".saveBtn").click(function(){
-        // console.log("click")
-        // make variables for the notes and hours and save to localstorage
-        var scheduleNotes = $(this).siblings(".description").val();
-        var hours = $(this).parent().attr("id");
-        localStorage.setItem(scheduleNotes, hours);
-        
+        // save plan and clock into locacl storage.
+        localStorage.setItem(clock, Plan);
+    })
+   
+    function timeTracker() {
+        // var for curren hour using moment().hour();
+        var currentHour = moment().hour();
 
-    });
-    // using loop function over the timeline of the schdule to identify past, present and future time
-    // past, present and future time can identify by different color
-    // past = pink, future = green, present = lightblue
-    function scheduleTimeline() {
-        var currenttime = moment().hour();
-        $(".time-block").each(function() {
-            var timeinBlock = parseInt($(this).attr("id").split("hour")[1]);
-        // .removeClass and .addClass used to create new class for identification if the current block time is past/future/present
-        if (timeinBlock < currenttime) {
-            // if currentime < timeinBlock => identify as past time with color lightblue
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        } else if (currenttime === timeinBlock) {
-            $(this).removeClass("future");
-            $(this).removeClass("past");
-            $(this).addClass("present");
-        } else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-        }
+        // Logic function to identify past/future/present.
+
+        $(".time-block").each(function () {
+            var Time = parseInt($(this).attr("id").split("hour")[1]);
+
+            // past will indicate pink background color.
+            // future will indicate green background color.
+            // present will indicate lightblue background color.
+            if (Time < currentHour) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (Time === currentHour) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
         })
-        
-        
-
     }
-    // SAVED notes into localstorage if any. 
+
+    // When page is refreshed, saved description will persist.
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
@@ -54,6 +56,6 @@ $(document).ready(function() {
     $("#hour15 .description").val(localStorage.getItem("hour15"));
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
-
-    scheduleTimeline();
+    // recall funcion.
+    timeTracker();
 })
