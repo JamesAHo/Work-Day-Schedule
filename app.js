6c09cd94ed717a874ed3efaf87c317f8ef0c82f5
James Anh <james.anh184@gmail.com>
2022-05-31 17:21:41 -0700

    changes

--- app.js
+++ app.js
@@ -18,19 +18,42 @@ $(document).ready(function() {
     });
     // using loop function over the timeline of the schdule to identify past, present and future time
     // past, present and future time can identify by different color
+    // past = pink, future = green, present = lightblue
     function scheduleTimeline() {
         var currenttime = moment().hour();
         $(".time-block").each(function() {
             var timeinBlock = parseInt($(this).attr("id").split("hour")[1]);
-        // .removeClass and .addClass used to create new class for identification
-        if (currenttime < timeinBlock) {
+        // .removeClass and .addClass used to create new class for identification if the current block time is past/future/present
+        if (timeinBlock < currenttime) {
             // if currentime < timeinBlock => identify as past time with color lightblue
-            $(this).removeClass()
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        } else if (currenttime === timeinBlock) {
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        } else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
         }
         })
         
         
 
     }
+    // SAVED notes into localstorage if any. 
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
     scheduleTimeline();
 })
